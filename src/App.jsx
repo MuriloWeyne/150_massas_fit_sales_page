901ad9ce3d17825bbe27fd3e64a93a816b0bbbf1
MuriloWeyne <muriloweyne1@gmail.com>
2026-02-20 00:07:55 -0300

    Implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import PastaFitLanding from './components/PastaFitLanding'
 
 function App() {
-  return <PastaFitLanding />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/massasfit" element={<PastaFitLanding />} />
+        <Route path="/" element={<Navigate to="/massasfit" replace />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
